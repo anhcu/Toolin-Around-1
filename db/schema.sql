74117673777fcab2f06a26017b73348bd7993cf7
Corey Parker <coreyparker012@gmail.com>
2021-07-31 11:38:09 -0400

    updated database name

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
 /* Replace template_db with your database */
 
-DROP DATABASE IF EXISTS template_db;
-CREATE DATABASE template_db;
+DROP DATABASE IF EXISTS toolshed_db;
+CREATE DATABASE toolshed_db;
