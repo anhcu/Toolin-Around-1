6802a6359fc7ff325ca2f98024e6a315b3d30271
osiyo <merewall@gmail.com>
2021-08-02 19:02:45 -0400

    route and logic for mytoolbox page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
+const { User, Category, Neighborhood, Tool } = require('../models');
 
 router.get('/', async (req, res) => {
   res.render('homepage');
@@ -13,8 +14,34 @@ router.get('/login', async (req, res) => {
   res.render('login');
 });
 
-router.get('/profile', async (req, res) => {
-  res.render('profile');
+router.get('/toolbox', withAuth, async (req, res) => {
+  try {
+    // FIND ALL TOOLS FROM THE LOGGED IN USER
+    // AND INCLUDE USER'S NAME FROM ASSOCIATION OF USER MODEL TO POST MODEL
+    const toolData = await Tool.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // SERIALIZE TOOL DATA SO TEMPLATE CAN READ IT
+    const tools = toolData.map((tool) => tool.get({ plain: true }));
+
+    // RENDER THE USER'S DASHBOARD WITH TOOLS & SESSION PARAMETERS FOR TEMPLATE
+    res.render('toolbox', { 
+      tools, 
+      logged_in: req.session.logged_in,
+      user_id: req.session.user_id,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 
