e0f0daf17a65b4b956110478fa61dee75971f865
osiyo <merewall@gmail.com>
2021-08-07 14:46:39 -0400

    fixing search bar feature so search is enabled from any page on app

--- public/js/search.js
+++ public/js/search.js
@@ -2,15 +2,18 @@ const searchHandler = async (event) => {
     event.preventDefault();
   
     const search = document.querySelector('#search').value.trim();
-    
+
+    // GRAB THE HOMEPAGE FILEPATH ONLY, REMOVING ANY ADDITIONAL ROUTES (I.E. /USERS ETC) FROM URL
+    const home_url = document.location.href.split("/").slice(0,3).join('/')
+
     if (search) {
-      const response = await fetch(`${search}`, {
+      const response = await fetch(`${home_url}/${search}`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        document.location.replace(`${search}`);
+        document.location.replace(`${home_url}/${search}`);
         console.log("all good!")
       } else {
         alert(response.statusText);
